55c9d22e5d560b2f4486beaa1a43f729c16f16bd
beatifulxu <1975668635@qq.com>
2018-06-04 22:28:44 +0800

    添加实例管理

--- student-service-master/studentservice-service/src/main/java/edu/zut/cs/studentservice/admin/service/impl/SecondGoodsManagerImpl.java
+++ student-service-master/studentservice-service/src/main/java/edu/zut/cs/studentservice/admin/service/impl/SecondGoodsManagerImpl.java
@@ -1,5 +1,19 @@
 package edu.zut.cs.studentservice.admin.service.impl;
 
-public class SecondGoodsManagerImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import edu.zut.cs.studentservice.admin.dao.SecondGoodsDao;
+import edu.zut.cs.studentservice.admin.domain.SecondGoods;
+import edu.zut.cs.studentservice.admin.service.SecondGoodsManager;
+import edu.zut.cs.studentservice.base.service.impl.GenericManagerImpl;
+
+@Component
+public class SecondGoodsManagerImpl extends GenericManagerImpl<SecondGoods,Long> implements SecondGoodsManager {
+	SecondGoodsDao Secondgoodsdaos;
+    @Autowired
+public void setSecondsDao(SecondGoodsDao Secondgoodsdaos) {
+    	this.Secondgoodsdaos=Secondgoodsdaos;
+    	this.dao=this.Secondgoodsdaos;
+    }
 }
