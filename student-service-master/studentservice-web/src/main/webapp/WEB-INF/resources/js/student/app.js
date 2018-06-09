831d08fa4e0ee860015e58fa588d147af3e43a64
duqingqing <dulovefighting.sina.com>
2018-06-09 10:51:11 +0800

    实现student的表格视图

--- student-service-master/studentservice-web/src/main/webapp/WEB-INF/resources/js/student/app.js
+++ student-service-master/studentservice-web/src/main/webapp/WEB-INF/resources/js/student/app.js
@@ -6,10 +6,10 @@ Ext.application({
 	appFolder : server_context + '/resources/js/student/app',
 	controllers : [ 'StudentController' ],
 	launch : function() {
-		Ext.create('Ext.container.Viewport', {
+		Ext.create('Ext.container.Viewport',{
 			layout : 'fit',
 			items : [ {
-				xtype : 'widget.studentGrid'
+				xtype : 'studentGrid'
 			} ]
 		});
 	}
